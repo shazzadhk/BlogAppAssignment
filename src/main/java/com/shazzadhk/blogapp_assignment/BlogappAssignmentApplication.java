82e512ad996c50b45164078fd99f7e1a395fe156
shazzadhk <shazzadhk12@gmail.com>
2022-12-11 15:40:46 +0600

    Create project and setup database

---
+++ src/main/java/com/shazzadhk/blogapp_assignment/BlogappAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.shazzadhk.blogapp_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogappAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogappAssignmentApplication.class, args);
+	}
+
+}
